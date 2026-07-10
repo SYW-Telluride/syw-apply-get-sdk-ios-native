3a06b7e3d67a08b3c90b36421614191455e6ce8a
Brijesh.Rashiya <brijesh.rashiya@syw.com>
2026-07-10 13:30:40 +0530

    first commit

---
+++ xcframeworks/ApplyGetCardSDK.xcframework/ios-arm64/ApplyGetCardSDK.framework/Headers/PywSDK.h
@@ -0,0 +1,18 @@
+//
+//  PywSDK.h
+//  PywSDK
+//
+//  Created by Viacheslav Pryimachenko on 13.07.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PywSDK.
+FOUNDATION_EXPORT double PywSDKVersionNumber;
+
+//! Project version string for PywSDK.
+FOUNDATION_EXPORT const unsigned char PywSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PywSDK/PublicHeader.h>
+
+
